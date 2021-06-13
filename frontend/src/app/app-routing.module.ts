a4cfc5c64a630e5b0e7fb6014e8dfd7a28c00a1b
lmorales2019047 <lmorales-2019047@kinal.edu.gt>
2021-06-12 21:05:47 -0600

    Se agregó vista "Registrarse"

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from "./components/signin/signin.component";
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   {
     path: "signin",
     component: SigninComponent
+  },{
+    path: "signup",
+    component: SignupComponent
   }
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
