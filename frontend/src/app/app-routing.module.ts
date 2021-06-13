ee24e4eb0b84aa14bd57691d12743c3398dded19
lmorales2019047 <lmorales-2019047@kinal.edu.gt>
2021-06-12 20:49:30 -0600

    Se creó vista "Iniciar Sesión"

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from "./components/signin/signin.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "signin",
+    component: SigninComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
