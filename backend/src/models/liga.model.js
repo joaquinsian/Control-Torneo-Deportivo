cb81e7c3c724399d52c71871fd0fe607fb5acf7e
unknown <jmorataya-2019041@kinal.edu.gt>
2021-06-21 13:12:08 -0600

    Se modificó el modelo de Liga

--- backend/src/models/liga.model.js
+++ backend/src/models/liga.model.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var LigaSchema = Schema({
-    nombre: String
+    nombre: String,
+    image: String,
+    creador: {type:Schema.Types.ObjectId, ref: 'usuario'}
 });
  
 module.exports = mongoose.model('liga', LigaSchema);
