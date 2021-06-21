39ec51611f21d7a033df3cd5f5e01d97d2c9b3c1
unknown <jmorataya-2019041@kinal.edu.gt>
2021-06-21 14:04:04 -0600

    Se editó la ruta de editar liga y se agregaron 2 rutas más

--- backend/src/routes/Liga.rutas.js
+++ backend/src/routes/Liga.rutas.js
@@ -7,9 +7,11 @@ var autentication = require('../middlewares/authenticated');
 var api = express.Router();
 
 api.post('/crearLiga', autentication.ensureAuth ,ligaController.createLiga);
-api.get('/mostrarLiga', ligaController.mostrarLiga);
+api.get('/mostrarLigas', ligaController.mostrarLigas);
+api.get('/ligasUser/:idUsuario', autentication.ensureAuth, ligaController.ligasForUser)
+api.get('/misLigas', autentication.ensureAuth, ligaController.misLigas)
 api.get('/mostrarLigaID/:idLiga', ligaController.mostrarLigaID);
-api.put('/editarLiga/:idLiga', autentication.ensureAuth , ligaController.editarLiga);
-api.delete('/eliminarLiga/:idLiga', autentication.ensureAuth, ligaController.eliminarLiga);
+api.put('/editarLiga/:idLiga', ligaController.editarLiga);
+api.delete('/eliminarLiga/:idLiga', ligaController.eliminarLiga);
 
 module.exports = api;
