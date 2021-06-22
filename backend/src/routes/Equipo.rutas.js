ddcb8c151360bad56f283b917570537a1b907a09
unknown <jmorataya-2019041@kinal.edu.gt>
2021-06-22 13:29:18 -0600

    Se agregó la ruta mostrar equipo por Id

--- backend/src/routes/Equipo.rutas.js
+++ backend/src/routes/Equipo.rutas.js
@@ -6,6 +6,7 @@ const equipoController = require('../controller/equipo.controller')
 var api = express.Router();
 
 api.post('/crearEquipo', equipoController.createEquipo);
-api.get('/mostrarEquipos', equipoController.mostrarEquipos)
+api.get('/mostrarEquipos', equipoController.mostrarEquipos);
+api.get('/equipoId/:idEquipo', equipoController.equipoId)
 
 module.exports = api;
