d80abddc8cec049399a870225ff7fdceab6fbe1e
unknown <jmorataya-2019041@kinal.edu.gt>
2021-06-23 14:07:10 -0600

    Se agregó la funciónd de editar tabla por Id de equipo

--- backend/src/controller/tabla.controller.js
+++ backend/src/controller/tabla.controller.js
@@ -1,5 +1,24 @@
 'use strict'
+var Tabla = require('../models/tabla.model')
+
+//Función para editar la tabla por id de equipo
+async function editarTabla(req, res){
+    var idEquipo = req.params.idEquipo;
+    var params = req.body;
+
+    await Tabla.findOneAndUpdate({equipo: idEquipo}, params, {new: true}, (err, tablaEditada)=>{
+        if(err){
+            return res.status(500).send({mensaje: "Error en la petición"})
+        }else if(!tablaEditada){
+            return res.status(500).send({mensaje: "No se ha podido editar la tabla"})
+        }else{
+            return res.status(200).send({tablaEditada})
+        }
+    })
+}
+
+//Función para editar la tabla por id de tabla
 
 module.exports = {
-    
+    editarTabla
 }
