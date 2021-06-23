436ff883360fbe4465a096df5f62cf07b2729bec
unknown <jmorataya-2019041@kinal.edu.gt>
2021-06-23 14:07:44 -0600

    Se agregó la ruta de editar tabla por Id de equipo

--- backend/src/routes/tabla.rutas.js
+++ backend/src/routes/tabla.rutas.js
@@ -3,5 +3,6 @@ const express = require('express');
 const tablaController = require('../controller/tabla.controller');
 
 var api = express.Router();
+api.put('/editarTabla/:idEquipo', tablaController.editarTabla)
 
 module.exports = api;
