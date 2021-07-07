b96fba868ab33b688d853eaa78d28b4638b40fe0
jmorataya2019041 <jmorataya-2019041@kinal.edu.gt>
2021-07-06 18:58:18 -0600

    Se agregó el modelo del resultado partido

---
+++ backend/src/models/resultado_partido.model.js
@@ -0,0 +1,11 @@
+'use strict'
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+var ResultadoPartidoSchema = Schema({
+    patido: {type: Schema.Types.ObjectId, ref: 'partidos'},
+    marcador_equipo_local: Number,
+    marcador_equipo_visitante: Number
+})
+
+module.exports = mongoose.model('resultados', ResultadoPartidoSchema)
