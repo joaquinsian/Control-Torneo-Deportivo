b3aed64c1b061a74d3ff57bde4baed334c1d6508
jmorataya2019041 <jmorataya-2019041@kinal.edu.gt>
2021-07-07 14:50:52 -0600

    Se agregó la ruta de obtener los partidos por liga

--- backend/src/routes/partidos.rutas.js
+++ backend/src/routes/partidos.rutas.js
@@ -4,5 +4,6 @@ const partidosController = require("../controller/partidos.controller")
 
 var api = express.Router();
 api.get('/generarPartidos/:idLiga', partidosController.generarPartidos)
+api.get('/obtenerPartidos/:idLiga', partidosController.obtenerPartidos)
 
 module.exports = api;
